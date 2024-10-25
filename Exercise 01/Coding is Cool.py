712d6396146515636e3195555022fd58ce45fe06
gelly0 <angelleonorgala@gmail.com>
2024-10-25 17:13:15 +0400

    Coding is Cool

--- Exercise 01/Coding is Cool.py
+++ Exercise 01/Coding is Cool.py
@@ -1,11 +1,19 @@
+''' 
 Exercise 1: Coding is Cool- 10 Marks
 
 Fill in the blanks in the Python code below to output the phrase “Coding is
 Cool” to the console using variables and string concatenation.
 
 Fill in the blanks below
+word1 =
+word2 =
+word3 =
+# Use string concatenation to combine the variables and print the phrase
+_____(_________) 
+'''
 
 # Use string concatenation to combine the variables and print the phrase
+
 word1 = "Coding" #variable 1
 word2 = "is" #variable 2
 word3 = "Cool" #variable 3
