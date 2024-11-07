a8ffda6591b9291684d8179cf8993fca55ee969d
gelly0 <angelleonorgala@gmail.com>
2024-11-07 22:50:00 +0400

    Advanced Requirements

---
+++ Exercise 04/Advanced Requirements.py
@@ -0,0 +1,97 @@
+'''
+Advanced Requirements:
+Ignore Capitalization: Modify your program to accept answers regardless of
+the capitalization (e.g., “paris”, “Paris”, and “PaRis” should all be considered
+correct). Multiple Questions: Extend the program into a quiz that asks for the
+capitals of 10 European countries. Provide feedback for each question.
+'''
+
+# 1st country
+france = str(input("What is the capital of France? "))
+# assigning if statement
+if france.strip().lower() == "paris":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Paris")
+
+# 2nd country
+germany = str(input("What is the capital of Germany? "))
+# assigning if statement
+if germany.strip().lower() == "berlin":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Berlin")
+
+# 3rd country
+italy = str(input("What is the capital of Italy? "))
+# assigning if statement
+if italy.strip().lower() == "rome":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Rome")
+
+# 4th country
+netherlands = str(input("What is the capital of Netherlands? "))
+# assigning if statement
+if  netherlands.strip().lower() == "amsterdam":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Amsterdam")
+
+# 5th country
+greece = str(input("What is the capital of Greece? "))
+# assigning if statement
+if  greece.strip().lower() == "athens":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Athens")
+
+# 6th country
+austria = str(input("What is the capital of Austria? "))
+# assigning if statement
+if  austria.strip().lower() == "vienna":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Vienna")
+
+#  7th country
+hungary = str(input("What is the capital of Hungary? "))
+# assigning if statement
+if  hungary.strip().lower() == "budapest":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Budapest")
+
+# 8th country
+denmark = str(input("What is the capital of Denmark? "))
+# assigning if statement
+if  denmark.strip().lower() == "copenhagen":
+    print ("Your answer is Correct!")
+
+else:
+    print ("The correct answer is Copenhagen")
+
+# 9th country
+finland = str(input("What is the capital of Finland? "))
+# assigning if statement
+if  finland.strip().lower() == "helsinki":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Helsinki")
+
+#10th country
+belgium = str(input("What is the capital of Belgium? "))
+# assigning if statement
+if  belgium.strip().lower() == "brussels":
+    print ("Your answer is Correct!")
+# assigning else statement
+else:
+    print ("The correct answer is Brussels")
