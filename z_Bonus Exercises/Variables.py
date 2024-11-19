5041b2105d1ad0d2aaeb5ddcb4be646dcf6bd92e
gelly0 <angelleonorgala@gmail.com>
2024-11-19 23:41:58 +0400

    Variables

---
+++ z_Bonus Exercises/Variables.py
@@ -0,0 +1,13 @@
+'''
+Variables: 
+Assign a message to a variable, and print that message. Then change the value 
+of the variable to a new message, and print the new message.
+'''
+
+# Assign an initial message to a variable
+intro = "Hello!"
+print (intro)
+
+# Change the value of the variable to a new message
+message = "Good Morning!"
+print (message)
