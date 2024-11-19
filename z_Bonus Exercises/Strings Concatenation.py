63aa11c4940936954a383dc773c395980059d6fb
gelly0 <angelleonorgala@gmail.com>
2024-11-19 23:40:42 +0400

    Strings Concatenation

---
+++ z_Bonus Exercises/Strings Concatenation.py
@@ -0,0 +1,11 @@
+'''
+Strings Concatenation: 
+Write three strings in different variables and print the output as one string.
+'''
+
+# Define three strings
+str1 = "Welcome"
+str2 = "to"
+str3 = "Bathspa University"
+
+print (str1 + " " + str2 + " " + str3)
