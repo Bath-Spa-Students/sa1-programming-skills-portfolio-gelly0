d79871b4a03b78a6f92c888c0e6aa9dc2ffe3568
gelly0 <angelleonorgala@gmail.com>
2024-11-19 23:36:01 +0400

    Chapter 2- Variables & Comments

---
+++ Class Activities/Chapter 2- Variables & Comments.py
@@ -0,0 +1,63 @@
+# Introduction about me
+print ("About me")
+print ("my name is Angel Gala") #name
+print ("l am 18 years old") #age
+print ("l live in Sharjah") #location
+print ("l am from the Philippines") #home country
+print ("my birthday is on 1 November 2006") #birthday
+print ("l love learning new instruments") #hobbies
+print ("my favorite color is purple") #color
+print ("my favorite animal is a turtle") #favorite animal
+print ("my flowers are tulips") #flower
+print ("l study in Bath Spa University") #university
+print ("my father's name is Alan Gala") #father
+print ("my mother's name is Nenia Gala") #mother
+
+# Welcome
+""" welcome to my first program
+in bathspa university """
+# Single line comment
+""" multiple line comment """
+
+print (""" Hello everyone
+       welcome to my introduction""") 
+print (""" wheels on the bus
+      goes round and round """) 
+print (""" happy
+       birthday """) 
+
+# Variables
+my_name = "hi! my name is Angel"
+A = "l am from the Philippines"
+B = "my birthday is on november 1"
+C = "and l love reading books"
+
+my_age = 17
+x = 20
+y = 45
+Z = 1.8
+print (type(x))
+print (type (y))
+print (type (Z))
+
+print (my_name, A, B, C)
+print (my_age)
+print (x)
+print (y)
+print (Z)
+
+x = 17, 14
+a = 10 
+b = 5
+y = "Angel"
+D = "I study at Bathspa University"
+P = "Purple"
+print (type (x))
+print (type (a))
+print (type (b))
+print (type (y))
+print (type (D))
+print (type (P))
+
+a = 0.8
+print (type (a))
