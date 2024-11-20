71bda8c5b9bb606f6a8a4ab6f9709271f466e6a5
gelly0 <angelleonorgala@gmail.com>
2024-11-20 22:20:15 +0400

    Operators

---
+++ Class Activities/Operators.py
@@ -0,0 +1,29 @@
+# Get Input from user.
+color = input("What is your favorite color? ")
+print("My favorite color is ", color)
+
+age = int(input("What is your age?"))
+print("My age is", age)
+
+a = 2
+b = 2
+c = 2
+print(a+b+c)
+
+# Perfoming Calculations
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)    #answer in floating point
+print(5//2)   #answer in integer
+
+v= int(input("enter a number: "))
+b= int(input("enter another number: "))
+print(v+b)
+
+# Operator precedence BODMAS
+print((2*6)/8 + 5*(8/4))
+
+# To write formula in multiple lines we need a back slash \
+print(8*3+3+2-8+ \
+    8/9-7*6)
