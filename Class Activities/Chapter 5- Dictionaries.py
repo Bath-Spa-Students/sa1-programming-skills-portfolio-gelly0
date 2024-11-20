8f7a58904582a7a4c91433b0e726a177cc49cfc6
gelly0 <angelleonorgala@gmail.com>
2024-11-20 19:27:11 +0400

    Chapter 5- Dictionaries

---
+++ Class Activities/Chapter 5- Dictionaries.py
@@ -0,0 +1,8 @@
+# To add something in dictionary
+example = {'color' : 'red' ,
+           'fruit' : 'apple' ,
+           'place' : 'United Arab Emirates '}
+print (example)
+
+dic = {'Name': 'Angel' , 'Age': 18}
+print (dic["Name"])
