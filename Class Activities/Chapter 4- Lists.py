1ed5c4ed93886003b6cf00c39fabb14e0dbe60f3
gelly0 <angelleonorgala@gmail.com>
2024-11-20 18:04:31 +0400

    Chapter 4- Lists

---
+++ Class Activities/Chapter 4- Lists.py
@@ -0,0 +1,27 @@
+# Homoheneous List 
+colors = ["purple", "red", "blue"]
+print (colors)
+
+ages = [16, 17, 13]
+print (ages)
+
+
+# Hetrogeneous List
+dates = [14, "january" , 2.13]
+print (dates)
+
+numbers = [1400 , "ashley" , 3000 , "johm"]
+print (numbers)
+
+# Repitition Operator * To repeat data
+months = ["january" , "february" , "march" , "april"]
+new_months = months * 3
+print (new_months)
+
+names = ["bathspa", "angel" , "sharjah"]
+new_names = names * 2
+print (new_names)
+
+rainbow = ["red" , "orange" , "yellow" , "green" ,"blue" , "violet"]
+colors = rainbow * 4
+print (colors)
