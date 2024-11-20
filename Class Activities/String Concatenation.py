042b27dba7d581a229bdac5936724bd2815775e2
gelly0 <angelleonorgala@gmail.com>
2024-11-20 22:20:56 +0400

    String Concatenation

---
+++ Class Activities/String Concatenation.py
@@ -0,0 +1,29 @@
+# String concatenation through one variable
+introduction = "bathspa " + "university"
+print (introduction)
+
+name = "Angel " + "Gala"
+print (name)
+
+color = "Black " + "pink"
+print (color)
+
+print ('hello ' + 'today ' + 'is thursday!')
+print ('Welcome ' + 'to Bathspa ' + 'University')
+print ('My name ' + 'is ' + 'Angel')
+
+# String concatenation through multiple variables
+a = "Welcome"
+b = "to Bathspa University"
+c = a + " " + b
+print (c)
+
+d = "My name"
+e = "is Angel"
+f = d + " " + e
+print (f)
+
+g = "l love"
+h = "the color purple"
+i = g + " " + h
+print (i)
